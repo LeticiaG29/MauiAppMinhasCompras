3a42ee79af5463730ca9f24018c6c1e15a0eaa97
Leticia Gouvêa <Leticia.g.s23@outlook.com>
2025-03-18 21:41:58 -0300

    validação da inserção de produto e correção dos produtos duplicados na tela inicial

--- MauiAppMinhasCompras/Models/Produto.cs
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -4,9 +4,20 @@ namespace MauiAppMinhasCompras.Models
 {
     public class Produto
     {
+        string _descricao;
+
         [PrimaryKey,AutoIncrement]
         public int Id { get; set; }
-        public string Descricao { get; set; }
+        public string Descricao {
+            get => _descricao;
+            set {
+                if (value == null) 
+                {
+                    throw new Exception("Por favor, Preencha a descrição");
+                }
+                _descricao = value;
+                }
+            }
         public double Quantidade { get; set; }
         public double Preco {  get; set; }
         public double Total { get => Quantidade * Preco; }
