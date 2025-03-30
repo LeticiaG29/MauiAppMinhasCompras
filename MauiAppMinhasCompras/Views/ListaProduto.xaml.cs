27fa72cb8ff5d127dc25786e7d3b7a166a7c9cba
Leticia Gouvêa <Leticia.g.s23@outlook.com>
2025-03-30 20:39:48 -0300

    Mudança no layout e  adicionando categoria

--- MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
@@ -6,6 +6,7 @@ namespace MauiAppMinhasCompras.Views;
 public partial class ListaProduto : ContentPage
 {
     ObservableCollection<Produto> lista = new ObservableCollection<Produto>();
+
     public ListaProduto()
 
     {
@@ -49,6 +50,8 @@ public partial class ListaProduto : ContentPage
         {
             string q = e.NewTextValue;
 
+            lst_produtos.IsRefreshing = true;
+
             lista.Clear();
 
             List<Produto> tmp = await App.Db.Search(q);
@@ -58,6 +61,10 @@ public partial class ListaProduto : ContentPage
         catch (Exception ex)
         {
           await DisplayAlert("Ops", ex.Message, "OK");
+        }
+        finally
+        {
+            lst_produtos.IsRefreshing = false;
         }
 
     }
@@ -109,5 +116,52 @@ public partial class ListaProduto : ContentPage
            DisplayAlert("Ops", ex.Message, "OK");
         }
 
+    }
+
+    private async void lst_produtos_Refreshing(object sender, EventArgs e)
+    {
+        try
+        {
+            lista.Clear();
+
+            List<Produto> tmp = await App.Db.GetAll();
+
+            tmp.ForEach(i => lista.Add(i));
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "OK");
+        }
+        finally
+        {
+            lst_produtos.IsRefreshing = false;
+        }
+    }
+
+    private async void picker_categoria_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        try
+        {
+            string categoriaSelecionada = picker_categoria.SelectedItem?.ToString();
+            lista.Clear();
+
+            List<Produto> produtosFiltrados;
+
+            if (categoriaSelecionada == "Todos")
+            {
+                produtosFiltrados = await App.Db.GetAll();
+            }
+            else
+            {
+                produtosFiltrados = await App.Db.GetAll();
+                produtosFiltrados = produtosFiltrados.Where(p => p.Categoria == categoriaSelecionada).ToList();
+            }
+
+            produtosFiltrados.ForEach(i => lista.Add(i));
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "OK");
+        }
     }
 }
